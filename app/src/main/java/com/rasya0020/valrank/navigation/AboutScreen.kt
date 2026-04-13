640ab78fa1461ba2e8e392ead5490ab935ab66e6
Mohammad Rasya Maessa Nugraha <rasyanugraha95@gmail.com>
2026-04-13 21:00:26 +0700

    Membuat AboutScreen

--- app/src/main/java/com/rasya0020/valrank/navigation/AboutScreen.kt
+++ app/src/main/java/com/rasya0020/valrank/navigation/AboutScreen.kt
@@ -1,23 +1,38 @@
 package com.rasya0020.valrank.navigation
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.wear.compose.material3.MaterialTheme
+import com.rasya0020.valrank.R
 import com.rasya0020.valrank.ui.theme.ValRankTheme
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun AboutScreenPreview() {
-    ValRankTheme() {
+    ValRankTheme{
         AboutScreen(rememberNavController())
     }
 }
@@ -26,12 +41,46 @@ fun AboutScreenPreview() {
 @Composable
 fun AboutScreen(navController: NavHostController){
     Scaffold(
-        topBar = {}
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
+                title = {
+                    Text(text = stringResource(id = R.string.tentang_aplikasi))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
-        ) { }
+                .verticalScroll(rememberScrollState())
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.desc_aplikasi),
+                style = androidx.compose.material3.MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Justify
+            )
+
+            Text(
+                text = stringResource(R.string.copyright),
+                style = androidx.compose.material3.MaterialTheme.typography.labelMedium,
+                color = androidx.compose.material3.MaterialTheme.colorScheme.outline
+            )
+        }
     }
 }
